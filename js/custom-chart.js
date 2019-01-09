ae359e8562e02bb59aeb796bf659d43d251691df
Gustavo Torres <gtvtorres@gmail.com>
2019-01-09 12:13:17 -0200

    Layout design HTML, CSS

--- js/custom-chart.js
+++ js/custom-chart.js
@@ -2,17 +2,33 @@ var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Blue", "Purple", "Purple Light", "Green"],
+        labels: ["JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"],
         datasets: [{
             label: 'Teste de Gráfico 01',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3],
             backgroundColor: [
+                '#00618b',
+                '#590052',
+                '#97138b',
+                '#00be82',
+                '#00618b',
+                '#590052',
+                '#97138b',
+                '#00be82',
                 '#00618b',
                 '#590052',
                 '#97138b',
                 '#00be82'
             ],
             borderColor: [
+                '#00618b',
+                '#590052',
+                '#97138b',
+                '#00be82',
+                '#00618b',
+                '#590052',
+                '#97138b',
+                '#00be82',
                 '#00618b',
                 '#590052',
                 '#97138b',
@@ -36,17 +52,21 @@ var ctx = document.getElementById("myChart2");
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue"],
+        labels: ["EXTRA PESADO", "LEVE", "MEDIO", "MEIO PESADO"],
         datasets: [{
             label: 'Teste de Gráfico 02',
-            data: [12, 50],
+            data: [30, 80, 50, 100],
             backgroundColor: [
                 '#00618b',
-                '#590052'
+                '#590052',
+                '#97138b',
+                '#00be82'
             ],
             borderColor: [
                 '#00618b',
-                '#590052'
+                '#590052',
+                '#97138b',
+                '#00be82'
             ],
             borderWidth: 1
         }]
