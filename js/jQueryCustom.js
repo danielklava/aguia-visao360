cab3fce84ccdaa79156d8575359d9db9a7805341
Gustavo Torres <gtvtorres@gmail.com>
2019-01-10 11:27:50 -0200

    Layout design HTML, CSS

--- js/jQueryCustom.js
+++ js/jQueryCustom.js
@@ -5,11 +5,13 @@ $(".btn-link:nth-child(1)").addClass("active");
 $(".btn-link:nth-child(2)").click(function(){
     $(".btn-link:nth-child(1)").removeClass("active");
     $(this).addClass("active");
+    $(".number-perc").html("55");
 })
 
 $(".btn-link:nth-child(1)").click(function(){
     $(".btn-link:nth-child(2)").removeClass("active");
     $(this).addClass("active");
+    $(".number-perc").html("78");
 })
 
 });
